d242146312c0d0a5faa554c685b62a33d826bb3d
HeyMilkshake <38050116+msmilkshake@users.noreply.github.com>
2022-10-24 14:10:00 +0100

    Update Compras.java

--- Compras.java
+++ Compras.java
@@ -12,5 +12,19 @@ public class Compras{
 			System.out.println("Error: " + e.getMessage());
 		}
 		
+		System.out.print("Insert time in seconds: ");
+		int time = Integer.parseInt(scanner.nextLine());
+		System.out.print("Insert distance in kilometers: ");
+		double distance = Double.parseDouble(scanner.nextLine());
+		int minutes = (int)(time / distance / 60);
+		int seconds = (int)(((time / distance / 60) - minutes) * 60);
+		System.out.printf("%02d:%02d/km", minutes, seconds);
+		System.out.print("\nPress ENTER to exit");
+		scanner.nextLine();
+		
+		System.out.printf("%02d:%02d/km", minutes, seconds);
+		System.out.print("\nPress ENTER to exit");
+		scanner.nextLine();
+		
 	}
 }
